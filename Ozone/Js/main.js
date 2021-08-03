000273bb6429c8def7a39ad155ba407d8973656f
kuluong2000 <tranhuuluong156200@gmail.com>
2021-08-03 23:07:53 +0700

    ud

--- Ozone/Js/main.js
+++ Ozone/Js/main.js
@@ -1,9 +1,17 @@
-const bars = document.querySelector('.sub-menu');
+
+function menuToggle(){
+    const bars = document.querySelector('.sub-menu');
 const menu = document.querySelector('.header-menu-list');
 const navlinks = document.querySelectorAll('.header-menu-list li');
 bars.addEventListener('click', ()=>{
     menu.classList.toggle('isactive');
     bars.classList.toggle('isactive');
+    window.addEventListener('click', function(e){
+        if (!menu.contains(e.target) && !e.target.matches('.sub-menu')){
+            menu.classList.remove('isactive');
+            bars.classList.remove('isactive');
+        }
+    });
     navlinks.forEach((link, index) => {
         if(link.style.animation){
             link.style.animation = '';
@@ -13,3 +21,5 @@ bars.addEventListener('click', ()=>{
         }
     });
 });
+
+}
