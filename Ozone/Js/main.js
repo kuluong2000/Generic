ce453e076c70f28b34617735f94b806d9c4b9ba1
kuluong2000 <tranhuuluong156200@gmail.com>
2021-08-03 17:11:28 +0700

    ud

--- Ozone/Js/main.js
+++ Ozone/Js/main.js
@@ -1,6 +1,15 @@
 const bars = document.querySelector('.sub-menu');
 const menu = document.querySelector('.header-menu-list');
+const navlinks = document.querySelectorAll('.header-menu-list li');
 bars.addEventListener('click', ()=>{
     menu.classList.toggle('isactive');
     bars.classList.toggle('isactive');
+    navlinks.forEach((link, index) => {
+        if(link.style.animation){
+            link.style.animation = '';
+        }
+        else{
+            link.style.animation = `navLink 0.5s ease forwards ${index / 7 + 0.5}s`;
+        }
+    });
 });
